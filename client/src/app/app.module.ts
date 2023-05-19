92cdd0b15a7c4895872defae05988f7df6fc05b3
Sara Castillo <saracm181105@gmail.com>
2023-05-18 23:01:24 -0500

    Creation of the shopping cart component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ShoppingCartComponent } from './modules/payments/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
