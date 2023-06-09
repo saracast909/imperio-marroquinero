c34479bad2b74e39e2bf762a0687cc18cc135acc
Sara Castillo <saracm181105@gmail.com>
2023-06-09 05:28:04 -0500

    Gifts component creation

--- client/src/app/modules/user/components/gifts/gifts.component.ts
+++ client/src/app/modules/user/components/gifts/gifts.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class GiftsComponent {
 
+  gifts = [
+    {
+      name: 'Envio gratuito',
+      descripcion: 'Por compras superiores a $0',
+      codigo: 'EVGSC',
+      finicio: '21/07/2023',
+      ffinal: '10/08/2023',
+      paplica: 'Para todos los productos'
+    },
+    {
+      name: '15% de descuento',
+      descripcion: 'Por compras superiores a $0',
+      codigo: 'DSCCG',
+      finicio: '21/07/2023',
+      ffinal: '10/08/2023',
+      paplica: 'Para todos los productos'
+    },
+    {
+      name: '15% de descuento',
+      descripcion: 'Por compras superiores a $0',
+      codigo: 'QPYTGF',
+      finicio: '21/07/2023',
+      ffinal: '10/08/2023',
+      paplica: 'Para todos los productos'
+    }
+]
 }
